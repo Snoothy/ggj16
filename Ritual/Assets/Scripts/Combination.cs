292bde8058817e3a2bc965fbbac54b635313bbea
AwsomeArni <arnidat@gmail.com>
2016-01-30 00:23:18 +0100

    Created a test scene for combination features and a simple working system.

---
+++ Ritual/Assets/Scripts/Combination.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Combination {
+    public IngredientType[] ingredients;
+    public GameObject product;
+
+    // NOTE: This assumes that the ingredients are not altered during runtime.
+    private IngredientType bitmaskCache = 0;
+    private bool isCacheSet = false;
+
+    public IngredientType bitmask
+    {
+        get
+        {
+            IngredientType mask = 0;
+            if (isCacheSet)
+            {
+                mask = bitmaskCache;
+            }
+            else {
+                if (ingredients != null)
+                {
+                    foreach (IngredientType type in ingredients)
+                    {
+                        mask |= type;
+                    }
+                    bitmaskCache = mask;
+                    isCacheSet = true;
+                }
+                else
+                    throw new System.Exception("The list of ingredients cannot be null");
+            }
+            return mask;
+        }
+    }
+}
---
+++ Ritual/Assets/Scripts/EnterPool.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class EnterPool : MonoBehaviour {
+
+    private CombinationTable table;
+
+	// Use this for initialization
+	void Start () {
+        table = this.GetComponentInParent<CombinationTable>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        Item item = collision.gameObject.GetComponent<Item>();
+        if (item != null)
+        {
+            if (item is Ingredient)
+            {
+                table.OnEnterCollision(item as Ingredient);
+            } else if (item is Product)
+            {
+                table.OnEnterCollision(item as Product);
+            }
+        }
+    }
+}
