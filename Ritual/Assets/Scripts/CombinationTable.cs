5ffae0ad0aeec4637229e09c53b4e95f05cbf737
AwsomeArni <arnidat@gmail.com>
2016-01-30 16:41:00 +0100

    The combinator is now contained in the combination table.

--- Ritual/Assets/Scripts/CombinationTable.cs
+++ Ritual/Assets/Scripts/CombinationTable.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using LayerManagement.Action.Finite;
 using LayerManagement.Action.Infinite;
 
@@ -13,20 +14,53 @@ public class SuckDownAnimationConfiguration
 public class CombinationTable : MonoBehaviour {
 
     public Transform spawnProductAt;
-    public Combinator combinator;
     public Transform enterPoolParent;
+    public Postitwall postitwall;
+
+    public List<Combination> combinations = new List<Combination>();
+    private IngredientType currentPool = 0;
+
+    public void addIngredientToCombination(Item item)
+    {
+        IngredientType type = item is Ingredient ? (item as Ingredient).ingredientType : IngredientTypeTools.getRandomIngredientType();
+        Destroy(item.gameObject);
+        addIngredientToCombination(type);
+    }
+
+    public void addIngredientToCombination(IngredientType item)
+    {
+        currentPool |= item;
+        postitwall.addIngredient(item);
+        GameObject product;
+        if (isIngredientsAProduct(currentPool, out product))
+        {
+            currentPool = 0;
+            instantiateProduct(product);
+            postitwall.Clear();
+        }
+    }
+
+    private bool isIngredientsAProduct(IngredientType pool, out GameObject product)
+    {
+        int c = combinations.Count;
+        Debug.LogWarning("c=" + c);
+            Debug.LogWarning("pool=" + pool.ToString());
+        for (int i = 0; i < c; i++)
+        {
+            Combination combination = combinations[i];
+            Debug.LogWarning("combination=" + combination.bitmask.ToString());
+            if (combination.bitmask == pool)
+            {
+                product = combination.product;
+                Debug.Log(currentPool.ToString() + "==" + combination.bitmask.ToString());
+                return true;
+            }
+        }
+        product = null;
+        return false;
+    }
 
     public SuckDownAnimationConfiguration spinConfigurations;
-
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
     public void OnEnterCollision (Ingredient item)
     {
@@ -75,12 +109,13 @@ public class CombinationTable : MonoBehaviour {
         spinParent.AddComponent<MoveToVectorByTime>().runActionWith(new MoveToVectorByTimeInfo(spinConfigurations.downTime, Vector3.zero, true, 0));
 
         ScaleToByTime scale = item.gameObject.AddComponent<ScaleToByTime>();
-        scale.delegates += a => combinator.addIngredientToCombination(item);
+        scale.delegates += a => addIngredientToCombination(item);
         scale.runActionWith(new ScaleToByTimeInfo(spinConfigurations.downTime, Vector3.zero, 0));
     }
 
     public void instantiateProduct(GameObject product)
     {
+        Debug.LogWarning("new product" + product.name);
         Instantiate(product, spawnProductAt.position, Quaternion.identity);
     }
 }
