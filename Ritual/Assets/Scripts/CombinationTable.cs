428b06ffd29911def2d8be47c3fba784e5d4aca9
AwsomeArni <arnidat@gmail.com>
2016-01-30 19:58:03 +0100

    Bad products are now spawned on failed combinations.

--- Ritual/Assets/Scripts/CombinationTable.cs
+++ Ritual/Assets/Scripts/CombinationTable.cs
@@ -18,6 +18,9 @@ public class CombinationTable : MonoBehaviour {
     public Postitwall postitwall;
 
     public List<Combination> combinations = new List<Combination>();
+    public List<Product> badproducts = new List<Product>();
+    private List<Product> badproductsStack = null;
+
     private IngredientType currentPool = 0;
     private int noIngredients = 0;
 
@@ -27,6 +30,27 @@ public class CombinationTable : MonoBehaviour {
         Destroy(item.gameObject);
         postitwall.addIngredient(item);
         addIngredientToCombination(type);
+    }
+
+    private GameObject getBadProduct ()
+    {
+        if (badproductsStack == null)
+        {
+            badproductsStack = new List<Product>();
+        }
+        if (badproductsStack.Count == 0)
+        {
+            badproductsStack.AddRange(badproducts);
+        }
+
+        GameObject badProduct;
+
+        int i = Random.Range(0, badproductsStack.Count);
+
+        badProduct = badproductsStack[i].gameObject;
+        badproductsStack.RemoveAt(i);
+
+        return badProduct;
     }
 
     public void addIngredientToCombination(IngredientType item)
@@ -40,12 +64,13 @@ public class CombinationTable : MonoBehaviour {
             noIngredients = 0;
             instantiateProduct(product);
             postitwall.Clear();
-        } else if(noIngredients == 3)
+        } else if (noIngredients == 3)
         {
             currentPool = 0;
             noIngredients = 0;
             postitwall.Clear();
             Debug.LogWarning("No combination was found");
+            instantiateProduct(this.getBadProduct());
         }
     }
 
