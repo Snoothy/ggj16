8739dce72556f0de1323ccccb6447aee9748a1b2
Kai Ejler Rasmussen <kai.ejler.rasmussen@gmail.com>
2016-01-30 21:56:54 +0100

    Lookat player

---
+++ Ritual/Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtPlayer : MonoBehaviour {
+
+	Transform player, t;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		t = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion q = Quaternion.LookRotation (t.position - player.position);
+		t.rotation = Quaternion.Euler(new Vector3(0f, q.eulerAngles.y, 0f));
+	}
+}
