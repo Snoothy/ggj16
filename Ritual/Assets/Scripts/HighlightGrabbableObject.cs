8f072a6d5fd084b2c5c60e6898e8a0e60ab496f5
AwsomeArni <arnidat@gmail.com>
2016-01-30 16:29:02 +0100

    Highlighted now also works with sprites.

--- Ritual/Assets/Scripts/HighlightGrabbableObject.cs
+++ Ritual/Assets/Scripts/HighlightGrabbableObject.cs
@@ -1,12 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class MaterialGameObjectPair
+public class MaterialMeshPair
 {
     public Material material;
     public MeshRenderer item;
 
-    public MaterialGameObjectPair(Material material, MeshRenderer item)
+    public MaterialMeshPair(Material material, MeshRenderer item)
+    {
+        this.material = material;
+        this.item = item;
+    }
+}
+
+public class MaterialSpritePair
+{
+    public Material material;
+    public SpriteRenderer item;
+
+    public MaterialSpritePair(Material material, SpriteRenderer item)
     {
         this.material = material;
         this.item = item;
@@ -16,10 +28,12 @@ public class MaterialGameObjectPair
 [RequireComponent(typeof(GrabController))]
 public class HighlightGrabbableObject : MonoBehaviour {
 
-    public Material outlineMaterial;
+    public Material outlineMaterialMesh;
+    public Material outlineMaterialSprite;
 
     private Item highlightedObject;
-    private List<MaterialGameObjectPair> highlightedObjectOriginalMaterial;
+    private List<MaterialMeshPair> meshMaterialPairs;
+    private List<MaterialSpritePair> spriteMaterialPairs;
 
     private GrabController grabController;
 
@@ -30,30 +44,48 @@ public class HighlightGrabbableObject : MonoBehaviour {
 
     void LateUpdate ()
     {
-        if (highlightedObject != null)
+        GameObject grabbedObject = grabController.getGrabbedObject();
+        GameObject grabbableObject = grabController.GetGrabbableObject();
+        GameObject objectToHighlight = null;
+
+        if (grabbedObject != null)
         {
-            foreach (MaterialGameObjectPair pair in highlightedObjectOriginalMaterial)
+            objectToHighlight = grabbedObject;
+        } else if (grabbableObject != null)
+        {
+            objectToHighlight = grabbableObject;
+        }
+
+        if (highlightedObject != null && (objectToHighlight == null || objectToHighlight != highlightedObject.gameObject))
+        {
+            foreach (MaterialMeshPair pair in meshMaterialPairs)
+            {
+                pair.item.material = pair.material;
+            }
+            foreach (MaterialSpritePair pair in spriteMaterialPairs)
             {
                 pair.item.material = pair.material;
             }
             highlightedObject = null;
-            highlightedObjectOriginalMaterial = null;
+            meshMaterialPairs = null;
+            spriteMaterialPairs = null;
         }
-        if (grabController.getGrabbedObject() == null)
-        {
-            GameObject grabbableObject = grabController.GetGrabbableObject();
-            if (grabbableObject != null)
-            {
-                Item item = grabbableObject.GetComponent<Item>();
+        if (objectToHighlight != null) {
+            Item item = objectToHighlight.GetComponent<Item>();
+			if (item != null) {
                 highlightedObject = item;
-				if (highlightedObject != null) {
-                    highlightedObjectOriginalMaterial = new List<MaterialGameObjectPair>();
-                    foreach (MeshRenderer mesh in highlightedObject.GetComponentsInChildren<MeshRenderer>())
-                    {
-                        highlightedObjectOriginalMaterial.Add(new MaterialGameObjectPair(mesh.material, mesh));
-                        mesh.material = Instantiate(outlineMaterial);
-                    }
-				}
+                meshMaterialPairs = new List<MaterialMeshPair>();
+                spriteMaterialPairs = new List<MaterialSpritePair>();
+                foreach (MeshRenderer mesh in highlightedObject.GetComponentsInChildren<MeshRenderer>())
+                {
+                    meshMaterialPairs.Add(new MaterialMeshPair(mesh.material, mesh));
+                    mesh.material = Instantiate(outlineMaterialMesh);
+                }
+                foreach (SpriteRenderer spr in highlightedObject.GetComponentsInChildren<SpriteRenderer>())
+                {
+                    spriteMaterialPairs.Add(new MaterialSpritePair(spr.material, spr));
+                    spr.material = Instantiate(outlineMaterialSprite);
+                }
             }
         }
     }
