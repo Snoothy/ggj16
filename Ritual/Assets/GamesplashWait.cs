d4af2102bc07381c0a843aa7f4c303727b4b7b0b
AwsomeArni <arnidat@gmail.com>
2016-01-31 14:06:49 +0100

    The logo screen is now processed by wait time.

---
+++ Ritual/Assets/GamesplashWait.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class GamesplashWait : MonoBehaviour {
+
+	public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+	    this.GetComponent<LayerManagement.Action.Finite.WaitAction>().delegates = a => SceneManager.LoadScene(sceneName);
+    }
+}
